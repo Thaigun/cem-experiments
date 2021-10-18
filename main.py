d2dd777531c5e69208a6f500ac2a6e60de6798b3
Antti M <antti_rekisteroinnit@hotmail.com>
2021-10-18 18:22:51 +0300

    Copy and make small tweaks to the Gym play() function to make it work in a multi-agent setting for one of the players.

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import gym
-from gym.utils.play import play
+from multi_agent_play import play
 from griddly import GymWrapperFactory, gd
+
+def make_move(prev_obs, obs, action, rew, env_done, info):
+    print('Our turn to play')
 
 if __name__ == '__main__':
     wrapper = GymWrapperFactory()
     wrapper.build_gym_from_yaml('TestBed', 'griddly_descriptions/testbed1.yaml')
     # Match with the name of the env created with GymWrapper
     env = gym.make('GDY-TestBed-v0')
-    play(env, fps=10, zoom=2)
-    '''    
-    env.reset()
-    while True:
-        env.render(observer='global')
-    for step in range(1000):
-        env.render()
-        observation, reward, done, info = env.step(env.action_space.sample())
-        if done:
-            print("Episode finished after {} timesteps".format(step + 1))
-            break
-    env.close()
-    '''
+    play(env, fps=10, zoom=2, callback=make_move)
+    
