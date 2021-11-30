c8d11fafbbb00ea129eed5a83f37dbae1f90062f
Antti M <antti_rekisteroinnit@hotmail.com>
2021-11-30 14:59:28 +0200

    Decently fast CEM-algorithm with one round of bug fixes.

--- griddly_cem_agent.py
+++ griddly_cem_agent.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # Implement a simple function that hashes the numpy array and returns the hash
 def hash_obs(obs):
-    return hash(str(obs))
+    return hash(obs.tobytes())
 
 
 def build_action(action, player_id, player_count):
@@ -13,7 +13,13 @@ def build_action(action, player_id, player_count):
     return built_action
 
 
-def build_mapping(env, current_hash, mapping, steps_left, action_space, current_player, main_player, samples=1):
+def build_mapping(env, hash_decode, current_hash, mapping, steps_left, action_space, current_player, main_player, samples=1):
+    if  current_hash in mapping:
+        return
+
+    if current_hash not in hash_decode:
+        hash_decode[current_hash] = env
+
     if steps_left == 0 or current_hash in mapping:
         return
     
@@ -31,28 +37,27 @@ def build_mapping(env, current_hash, mapping, steps_left, action_space, current_
                 cpd_s_a[state_after_hash] = sample_weight
             else:
                 cpd_s_a[state_after_hash] += sample_weight
-            build_mapping(clone_env, state_after_hash, mapping, steps_left-1, action_space, current_player % env.player_count + 1, main_player, samples)
+            build_mapping(clone_env, hash_decode, state_after_hash, mapping, steps_left-1, action_space, current_player % env.player_count + 1, main_player, samples)
         mapping[current_hash][action_idx] = cpd_s_a
 
 
-def build_distribution(env, actions, action_stepper, action_space, for_player, current_step_player, steps, hash_decode, samples):
+def build_distribution(env_hash, mapping, actions, action_stepper, action_space, for_player, current_step_player, player_count, steps):
     if steps == 0:
-        hash = env.get_state()['Hash']
-        if hash not in hash_decode:
-            hash_decode[hash] = env
-        return {hash: 1.0}
+        return {env_hash: 1.0}
     
-    actions = [actions[action_stepper]] if for_player == current_step_player else [a for a in action_space]
+    # If this step is for the player whose empowerment is being calculated, take the next action from the actions list. Otherwise, we use all possible actions 0 ... len(action_space-1)
+    step_actions = [actions[action_stepper]] if for_player == current_step_player else range(len(action_space))
     next_probs = {}
-    coeff = 1 / len(actions)
-    for action_idx, action in enumerate(actions):
-        clone_env = env.clone()
-        obs, rew, env_done, info = clone_env.step(build_action(action, current_step_player, env.player_count))
-        next_distribution = build_distribution(clone_env, actions, action_stepper + 1, action_space, for_player, current_step_player % env.player_count + 1, steps-1, hash_decode, samples)
-        for key in next_distribution:
-            if key not in next_probs:
-                next_probs[key] = 0
-            next_probs[key] += next_distribution[key] * coeff
+    coeff = 1 / len(step_actions)
+    for action in step_actions:
+        next_hash = mapping[env_hash][action]
+        for hash_prob in next_hash.items():
+            next_action_step = action_stepper + 1 if for_player == current_step_player else action_stepper
+            next_distribution = build_distribution(hash_prob[0], mapping, actions, next_action_step, action_space, for_player, current_step_player % player_count + 1, player_count, steps-1)
+            for key in next_distribution:
+                if key not in next_probs:
+                    next_probs[key] = 0
+                next_probs[key] += next_distribution[key] * coeff * hash_prob[1]
     return next_probs
 
 
@@ -73,25 +78,29 @@ def cem_action(env, current_player, steps, empowerment_pairs, empowerment_weight
 
     expected_empowerments_per_pair = []
 
-    for emp_pair in empowerment_pairs:
+    anticipation_step_counts = [(emp_pair[0] - current_player if emp_pair[0] > current_player else env.player_count - (current_player - emp_pair[0])) for emp_pair in empowerment_pairs]
+    mapping = {}
+    hash_decode = {}
+    build_mapping(env, hash_decode, env.get_state()['Hash'], mapping, max(anticipation_step_counts) + steps * env.player_count, action_space, current_player, current_player, samples)
+
+    for pair_i, emp_pair in enumerate(empowerment_pairs):
         expected_empowerments = np.zeros(len(action_space))
-        hash_decode = {}
-        anticipation_step_count = emp_pair[0] - current_player if emp_pair[0] > current_player else env.player_count - (current_player - emp_pair[0])
+        anticipation_step_count = anticipation_step_counts[pair_i]
         anticipation = [None] * len(action_space)
         for action_idx, action in enumerate(action_space):
-            anticipation[action_idx] = build_distribution(env, [action], 0, action_space, current_player, current_player, anticipation_step_count, hash_decode, samples)
+            anticipation[action_idx] = build_distribution(env.get_state()['Hash'], mapping, [action_idx], 0, action_space, current_player, current_player, env.player_count, anticipation_step_count)
         # Calculate the n-step empowerment for each state that was found earlier
         reachable_states =[]
         for a in anticipation:
             for key in a:
                 reachable_states.append(key)
         # All possible action combinations of length 'step'
-        action_combinations = [list(combo) for combo in product(action_space, repeat=steps)]
+        action_combinations = [list(combo) for combo in product(range(len(action_space)), repeat=steps)]
         nstep_combo_probs = [None] * len(action_combinations)
         state_empowerments = {}
         for state in reachable_states:
             for combo_idx, action_combo in enumerate(action_combinations):
-                nstep_combo_probs[combo_idx] = build_distribution(hash_decode[state], action_combo, 0, action_space, emp_pair[0], emp_pair[0], steps * env.player_count, hash_decode, samples)
+                nstep_combo_probs[combo_idx] = build_distribution(state, mapping, action_combo, 0, action_space, emp_pair[0], emp_pair[0], env.player_count, steps * env.player_count)
             
             cpd_s_a = [{} for _ in nstep_combo_probs]
             for c_id, c in enumerate(nstep_combo_probs):
@@ -99,9 +108,8 @@ def cem_action(env, current_player, steps, empowerment_pairs, empowerment_weight
                     latest_obs = hash_decode[key]._player_last_observation[emp_pair[1]-1]
                     hashed_obs = hash_obs(latest_obs)
                     if hashed_obs not in cpd_s_a[c_id]:
-                        cpd_s_a[c_id][hashed_obs] = c[key]
-                    else:
-                        cpd_s_a[c_id][hashed_obs] += c[key]
+                        cpd_s_a[c_id][hashed_obs] = 0
+                    cpd_s_a[c_id][hashed_obs] += c[key]
             # Use the Blahut-Arimoto algorithm to calculate the optimal probability distribution
             pd_a_opt = empowerment_maximization.blahut_arimoto(cpd_s_a, np.random.default_rng())
             # Use the optimal distribution to calculate the mutual information (empowerement)
