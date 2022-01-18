c4bb5aa2363051f1fd58a568b972d5bd57be9c85
Antti M <antti_rekisteroinnit@hotmail.com>
2022-01-18 15:07:39 +0200

    Add a test case that showcases trust function or lack thereof.

---
+++ trust_test.py
@@ -0,0 +1,29 @@
+import os
+import cProfile
+from griddly import GymWrapperFactory, gd, GymWrapper
+from griddly_cem_agent import CEMEnv, find_player_pos
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from visualiser import plot_empowerment_landscape, build_landscape, emp_map_to_str
+
+if __name__ == '__main__':
+    wrapper = GymWrapperFactory()
+    name = 'trust'
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    env = GymWrapper(current_path + '/griddly_descriptions/testbed_trust.yaml',
+                     player_observer_type=gd.ObserverType.VECTOR,
+                     global_observer_type=gd.ObserverType.SPRITE_2D,
+                     level=0)
+
+    player_id = 2
+    emp_pairs = [(2,2), (2,1)]
+    
+    for nstep in range(1, 2):
+        print('nstep: ', nstep)
+        env.reset()
+        calculated_emps = build_landscape(env, player_id, emp_pairs, [[1,2]], nstep, [['move', 'idle', 'attack'], ['move', 'idle', 'attack']], 1, 1)
+        for emp_pair_i, emp_pair in enumerate(emp_pairs):
+            print(emp_map_to_str(calculated_emps[emp_pair_i]))
+            plot_empowerment_landscape(env, calculated_emps[emp_pair_i], 'Empowerment: ' + str(emp_pair))
+    
