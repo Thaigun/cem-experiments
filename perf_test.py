119fea9d2646212e977a540b8c91c7a71a12b237
Antti M <antti_rekisteroinnit@hotmail.com>
2022-02-03 02:34:09 +0200

    Adapt perf_test to use the separate conf_parser.

--- perf_test.py
+++ perf_test.py
@@ -1,11 +1,16 @@
 from griddly import gd, GymWrapper
 import cProfile
 import os
-from griddly_cem_agent import CEM, EmpConf
+
+from numpy import full
+from griddly_cem_agent import CEM
+import conf_parser
 
 if __name__ == '__main__':
+    conf_parser.activate_config('threeway')
+
     current_path = os.path.dirname(os.path.realpath(__file__))
-    env = GymWrapper(current_path + '/griddly_descriptions/testbed3.yaml',
+    env = GymWrapper(current_path + '/griddly_descriptions/' + conf_parser.active_config.get('GriddlyDescription'),
                      player_observer_type=gd.ObserverType.VECTOR,
                      global_observer_type=gd.ObserverType.SPRITE_2D,
                      level=0)
@@ -13,12 +18,12 @@ if __name__ == '__main__':
 
     # Profile a step with CEM agent
     with cProfile.Profile() as pr:
-        action_sets = [['idle', 'move', 'heal', 'attack'],['idle', 'move', 'heal', 'attack']]
-        configs = EmpConf([(1,1), (2,2), (2,1)], [1, 1, 1])
-        cem = CEM(env, {2: configs}, [[1],[2]], agent_actions=action_sets, samples=1)
+        cem = CEM(env, conf_parser.active_config['Agents'])
         for _ in range(1):
             action = cem.cem_action(env, 2, 2)
-            obs, rew, env_done, info = env.step([[0,0], list(action)])
+            full_action = [[0, 0] for _ in range(env.player_count)]
+            full_action[1] = action
+            obs, rew, env_done, info = env.step(full_action)
 
     pr.print_stats(sort='cumtime')
 
