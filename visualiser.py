efbfeed3a6bdd4d7c5a3cec0c773f88935384358
Antti M <antti_rekisteroinnit@hotmail.com>
2022-02-04 04:42:14 +0200

    Neat depth-first traversal for visualiser.

--- visualiser.py
+++ visualiser.py
@@ -48,41 +48,57 @@ def plot_empowerment_landscape(env, position_emps, title):
 
 
 def build_landscape(orig_env, player_id, agent_configs, n_step, samples=1, trust_correction=False):
-    def random_move_action(env, player_id):
+    def non_blocked_moves(env, player_id, visited_coords):
         available_actions = env.game.get_available_actions(player_id)
         player_pos = list(available_actions)[0]
         actions_to_ids = env.game.get_available_action_ids(player_pos, list(available_actions[player_pos]))
         # Find directions that are blocked because there are other players
-        blocked_move_ids = []
-        for i in range(1, env.player_count+1):
-            if i == player_id:
-                continue
-            other_plr_pos = find_player_pos_vanilla(env, i)
-            other_plr_dir = [other - this for other, this in zip(other_plr_pos, player_pos)]
-            if other_plr_dir == [1,0]:
-                blocked_move_ids.append(3)
-            elif other_plr_dir == [-1, 0]:
-                blocked_move_ids.append(1)
-            elif other_plr_dir == [0, 1]:
-                blocked_move_ids.append(4)
-            elif other_plr_dir == [0, -1]:
-                blocked_move_ids.append(2)
+        blocked_move_ids = set()
+        # Coordinates in which an object would block the player
+        blocking_coords = [
+            tuple(sum(xy) for xy in zip(player_pos, [-1,0])),
+            tuple(sum(xy) for xy in zip(player_pos, [0,-1])),
+            tuple(sum(xy) for xy in zip(player_pos, [1,0])),
+            tuple(sum(xy) for xy in zip(player_pos, [0,1])),
+        ]
+        env_state = env.get_state()
+        for coord in blocking_coords:
+            if coord in visited_coords:
+                blocked_move_ids.add(blocking_coords.index(coord) + 1)
+        for o in env_state['Objects']:
+            if tuple(o['Location']) in blocking_coords:
+                blocked_move_ids.add(blocking_coords.index(tuple(o['Location'])) + 1)
 
-        possible_action_combos = []
+        possible_move_actions = []
         if 'move' in actions_to_ids:
             for action_id in actions_to_ids['move']:
                 if action_id not in blocked_move_ids:
-                    possible_action_combos.append([env.gdy.get_action_names().index('move'), action_id])
-        rnd_action = random.choice(possible_action_combos)
-        full_action = [[0,0] for _ in range(env.player_count)]
-        full_action[player_id-1] = rnd_action
-        return full_action
+                    possible_move_actions.append([env.gdy.get_action_names().index('move'), action_id])
+        return possible_move_actions
 
-    def orientation_fix_action(env, player_id, orientation):
-        full_action = [[0,0] for _ in range(env.player_count)]
-        rotate_action_idx = env.action_names.index('rotate')
-        full_action[player_id-1] = [rotate_action_idx, orientation]
-        return full_action
+    def get_unmove_action(action):
+        counter_moves = {
+            1: 3,
+            2: 4,
+            3: 1,
+            4: 2,
+        }
+        return (action[0], counter_moves[action[1]])
+
+    def traverse(env, player_id, calculated_emps, visited_coords, emp_pairs, cem_env, orientation_fix_action):
+        plr_pos = find_player_pos_vanilla(env, player_id)
+        visited_coords.add(tuple(plr_pos))
+        for emp_pair_i, emp_pair in enumerate(emp_pairs):
+            state_emp = cem_env.calculate_state_empowerment(EnvHashWrapper(env.clone()), emp_pair[0], emp_pair[1], n_step, trust_correction=trust_correction)
+            calculated_emps[emp_pair_i][tuple(plr_pos)] = state_emp
+        for a in non_blocked_moves(env, player_id, visited_coords):
+            env.step(a)
+            env.step(orientation_fix_action)
+            env.render(observer='global')
+            traverse(env, player_id, calculated_emps, visited_coords, emp_pairs, cem_env, orientation_fix_action)
+            env.step(get_unmove_action(a))
+            env.step(orientation_fix_action)
+            env.render(observer='global')
 
     env = orig_env.clone()
     # We have to start with an idle step because the observations are not cloned and we need them.
@@ -90,23 +106,18 @@ def build_landscape(orig_env, player_id, agent_configs, n_step, samples=1, trust
     # Extract the original rotation of the player with given player_id
     player_rot_name = next(o['Orientation'] for o in env.get_state()['Objects'] if o['Name'] == 'plr' and o['PlayerId'] == player_id)
     player_rot = {'LEFT': 1, 'UP': 2, 'RIGHT': 3, 'DOWN': 4, 'NONE': 2}[player_rot_name]
-    orientation_fix = orientation_fix_action(env, player_id, player_rot)
+    
+    # Build an action that is used to fix the orientation after each step
+    orientation_fix = [[0,0] for _ in range(env.player_count)]
+    rotate_action_idx = env.action_names.index('rotate')
+    orientation_fix[player_id-1] = [rotate_action_idx, player_rot]
 
+    # Prepare for traversal
     emp_pairs = [(emp['Actor'], emp['Perceptor']) for emp in agent_configs[player_id-1]['EmpowermentPairs']]
     calculated_emps = [{} for _ in emp_pairs]
+    visited_coords = set()
     cem_env = CEM(env, agent_configs, samples=samples)
-    for _ in range(2000):
-        plr_pos = find_player_pos_vanilla(env, player_id)
-        if tuple(plr_pos) not in calculated_emps[0]:
-            for emp_pair_i, emp_pair in enumerate(emp_pairs):
-                state_emp = cem_env.calculate_state_empowerment(EnvHashWrapper(env.clone()), emp_pair[0], emp_pair[1], n_step, trust_correction=trust_correction)
-                calculated_emps[emp_pair_i][tuple(plr_pos)] = state_emp
 
-        # Find a random movement action that does not target a populated tile.
-        # This assumes there is a 'move' action with action_ids 1=LEFT, 2=UP, 3=RIGHT, 4=DOWN.
-        random_action = random_move_action(env, player_id)
-        env.step(random_action)
-        # Rotate the player to the original orientation
-        env.step(orientation_fix)
-        env.render(observer='global')
+    traverse(env, player_id, calculated_emps, visited_coords, emp_pairs, cem_env, orientation_fix)
+
     return calculated_emps
